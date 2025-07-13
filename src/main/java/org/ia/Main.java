3764f4d3ce8f41f0450af65c59c8a0444c646e08
joaopfduarte <joaopedrofduarte@outlook.com>
2025-07-13 13:25:57 -0300

    implementation of based perceptron

--- src/main/java/org/ia/Main.java
+++ src/main/java/org/ia/Main.java
@@ -1,17 +1,84 @@
 package org.ia;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static double[][][] baseI = {
+            {{0, 0}, {0}},
+            {{0, 1}, {0}},
+            {{1, 0}, {0}},
+            {{1, 1}, {1}}
+    };
+
+    public static double[][][] baseII = {
+            {{0, 0}, {0}},
+            {{0, 1}, {1}},
+            {{1, 0}, {1}},
+            {{1, 1}, {1}}
+    };
+
+    public static double[][][] baseIII = {
+            {{0, 0}, {0}},
+            {{0, 1}, {1}},
+            {{1, 0}, {1}},
+            {{1, 1}, {0}}
+    };
+
+    public static double[][][] baseIV = {
+            {{0, 0, 0}, {1, 1}},
+            {{0, 0, 1}, {0, 1}},
+            {{0, 1, 0}, {1, 0}},
+            {{0, 1, 1}, {0, 1}},
+            {{1, 0, 0}, {1, 0}},
+            {{1, 0, 1}, {1, 0}},
+            {{1, 1, 0}, {1, 0}},
+            {{1, 1, 1}, {1, 0}}
+    };
+
+    public static void main(String[] args) {
+        // Base E (AND)
+        Perceptron perceptronE = new Perceptron(2, 1);
+        for (int epoch = 0; epoch < 10000; epoch++) {
+            double errE = 0;
+            for (int a = 0; a < baseI.length; a++) {
+                double[] out = perceptronE.treinar(baseI[a][0], baseI[a][1]);
+                errE += Math.abs(out[0] - baseI[a][1][0]);
+            }
+            System.out.printf("[BASE E] Epoch: %d - Error: %.4f%n", epoch, errE);
+        }
+
+        // Base OU (OR)
+        Perceptron perceptronOU = new Perceptron(2, 1);
+        for (int epoch = 0; epoch < 10000; epoch++) {
+            double errOU = 0;
+            for (int a = 0; a < baseII.length; a++) {
+                double[] out = perceptronOU.treinar(baseII[a][0], baseII[a][1]);
+                errOU += Math.abs(out[0] - baseII[a][1][0]);
+            }
+            System.out.printf("[BASE OU] Epoch: %d - Error: %.4f%n", epoch, errOU);
+        }
+
+        // Base XOR
+        Perceptron perceptronXOR = new Perceptron(2, 1);
+        for (int epoch = 0; epoch < 10000; epoch++) {
+            double errXOR = 0;
+            for (int a = 0; a < baseIII.length; a++) {
+                double[] out = perceptronXOR.treinar(baseIII[a][0], baseIII[a][1]);
+                errXOR += Math.abs(out[0] - baseIII[a][1][0]);
+            }
+            System.out.printf("[BASE XOR] Epoch: %d - Error: %.4f%n", epoch, errXOR);
+        }
+
+        // Base Robô
+        Perceptron perceptronRobo = new Perceptron(3, 2);
+        for (int epoch = 0; epoch < 10000; epoch++) {
+            double errROBO = 0;
+            for (int a = 0; a < baseIV.length; a++) {
+                double[] out = perceptronRobo.treinar(baseIV[a][0], baseIV[a][1]);
+                for (int j = 0; j < baseIV[a][1].length; j++) {
+                    errROBO += Math.abs(out[j] - baseIV[a][1][j]);
+                }
+            }
+            System.out.printf("[BASE ROBO] Epoch: %d - Error: %.4f%n", epoch, errROBO);
         }
     }
 }
